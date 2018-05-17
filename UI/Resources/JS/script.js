c27612c273135f12c96823268da9595c1ce096bc
Johnpaul-Borree <bohrplus@gmail.com>
2018-05-17 02:53:11 +0100

    Update script.js

--- UI/Resources/JS/script.js
+++ UI/Resources/JS/script.js
@@ -1,9 +1,9 @@
 //Adding Media querry
 function mediaQuery(x) {
     if (x.matches) { // If media query matches
-        document.getElementById("notice").innerHTML = "<button>join Us</button> Making sure your <br> equipments are in good condition.";
+        document.getElementById("notice").innerHTML = "<a href='UI/create.html' target='_blank'>join Us</a> Making sure your <br> equipments are in good condition.";
     } else{
-    	document.getElementById("notice").innerHTML = "We have all it takes in making sure your <br> equipments are in good condition.<br>No more issues of faulty equipments,<br> you do not need to make a call <br>simply <button>get started &rarr;</button>"
+    	document.getElementById("notice").innerHTML = "We have all it takes in making sure your <br> equipments are in good condition.<br>No more issues of faulty equipments,<br> you do not need to make a call <br>simply <a href='UI/create.html' target='_blank'>get started &rarr;</a>"
     }
 }
 
