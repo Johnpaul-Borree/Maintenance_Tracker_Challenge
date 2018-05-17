8490cb5e827071510aecdeaa73d84a2c84daaa75
Johnpaul <bohrplus@gmail.com>
2018-05-18 00:28:38 +0100

    [#157678400] setup completed and required modules created

--- MAintenance_API/app.js
+++ MAintenance_API/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+//import routes
+const requestRoutes = require("./api/routes/requests");
+app.use("/requests", requestRoutes);
 
 app.use((req, res, next) => {
 	res.status(200).json({
 		message : "connected"
 	});
-	next();
+
 });
 
 
