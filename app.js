c6fae3e8ccc25cde718348e57d0a7d138419c29b
Johnpaul <bohrplus@gmail.com>
2018-05-22 14:41:35 +0100

    travis tests

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+//import routes
+const requestRoutes = require("./api/routes/requests");
+
+app.use("/users/requests", requestRoutes);
+
+app.set("json spaces",2);
+
+app.use((req, res) => {
+	res.status(200).json({
+		message : "connected"
+	});
+
+});
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+
+	console.log(`listening to port ${port}`);
+});
+
+module.exports = app;
