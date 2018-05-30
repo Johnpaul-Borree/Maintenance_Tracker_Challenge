221e814c970d446225ab038afca63adeb6652197
Johnpaul <bohrplus@gmail.com>
2018-05-30 18:17:24 +0100

    feature (Admin approve requests) #157963087

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,28 +4,37 @@ const router = express.Router();
 
 router.use(express.json());
 
-import { getAllRequests, getRequests, signUp, postRequests, getRequestsById, updateRequests, login, verifyToken, authenticateUser } from "../controllers/usersController";
+import { /*resolveRequest, disapproveRequest, */approveRequest, getAdmin, getAllRequests, getRequests, signUp, postRequests, getRequestsById, updateRequests, login, verifyToken, authenticateUser } from "../controllers/usersController";
 
 //POST: /api/v1/users/signUp
 router.post("/auth/signup", signUp);
 
-//POST: /api/v1/users/login
+//POST: /api/v1/auth/login
 router.post("/auth/login", login);
 
-//GET: /api/users/requests/userId
+//GET: /api/v1/users/requests/userId
 router.get("/users/requests", verifyToken, authenticateUser, getRequests);
 
-//POST:/api/users/request
+//POST:/api/v1/users/request
 router.post("/users/requests", verifyToken, authenticateUser, postRequests);
 
-//GET: /api/users/requests/id
+//GET: /api/v1/users/requests/id
 router.get("/users/requests/:id", verifyToken, authenticateUser, getRequestsById);
 
-//PUT /api/users/requests/id
+//PUT /api/v1/users/requests/id
 router.put("/users/requests/:id", verifyToken, authenticateUser, updateRequests);
 
 //GET /api/v1/requests
-router.get("/requests", getAllRequests);
+router.get("/requests", verifyToken, authenticateUser, getAdmin, getAllRequests);
+
+//PUT: /api/v1/requests/<requestId>/approve
+router.put("/requests/:id/approve", verifyToken, authenticateUser, getAdmin, approveRequest);
+
+//PUT: /api/v1/requests/<requestId>/approve
+// router.put("/requests/:id/disapprove", verifyToken, authenticateUser, getAdmin, disapproveRequest);
+
+//PUT: /api/v1/requests/<requestId>/approve
+// router.put("/requests/:id/resolve", verifyToken, authenticateUser, getAdmin, resolveRequest);
 
 
 export default router;
