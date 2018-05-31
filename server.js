99efb00c3e0958953717dd1b5638f5d0baef267d
Johnpaul <bohrplus@gmail.com>
2018-05-31 22:36:30 +0100

    feedback

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/api/v1", requestRoutes);
 
+app.all("*",(req,res) => {
+	res.status(404).send({
+		message: "Page not Found"
+	});
+});
 // app.set("json spaces",2);
 
 const port = process.env.PORT || 3000;
